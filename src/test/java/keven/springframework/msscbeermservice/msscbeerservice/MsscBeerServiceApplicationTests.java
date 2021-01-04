7683746ea7830939f10f19f9f45edd1be6256562
KevenWen <10082511@qq.com>
2021-01-04 08:01:32 +0800

    initial project add. closes #1

---
+++ src/test/java/keven/springframework/msscbeermservice/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package keven.springframework.msscbeermservice.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
