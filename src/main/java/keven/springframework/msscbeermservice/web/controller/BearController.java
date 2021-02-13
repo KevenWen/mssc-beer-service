d13a0943936e828b91757e3c3f26671339b969bd
KevenWen <10082511@qq.com>
2021-02-13 20:39:20 +0800

    adding controller. Closes #1

---
+++ src/main/java/keven/springframework/msscbeermservice/web/controller/BearController.java
@@ -0,0 +1,35 @@
+package keven.springframework.msscbeermservice.web.controller;
+
+import keven.springframework.msscbeermservice.web.mode.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BearController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerByid(@PathVariable("beerId")  UUID beerId){
+
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+
+        //todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId")  UUID beerId, @RequestBody BeerDto beerDto){
+
+        //todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+}
