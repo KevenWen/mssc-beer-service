fb1504d34b3df76b78da7a5e1d628f3aa4145478
KevenWen <10082511@qq.com>
2021-02-14 16:58:30 +0800

    Added Beer Entiry.

--- src/test/java/keven/springframework/msscbeermservice/web/controller/BearControllerTest.java
+++ src/test/java/keven/springframework/msscbeermservice/web/controller/BearControllerTest.java
@@ -1,25 +1,38 @@
 package keven.springframework.msscbeermservice.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BearController.class)
+@Slf4j
 class BearControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
-    @Test
-    void getBeerByid() {
+    @Autowired
+    ObjectMapper objectMapper;
 
-      //  mockMvc.perform(get());  //(get("/api/v1/beer/" + UUID.randomUUID().toString()).accpt)
+//    @MockBean
+//    BeerService beerService;
+
+    @Test
+    void getBeerByid() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+                //.andDo();
     }
+
 
     @Test
     void saveNewBeer() {
